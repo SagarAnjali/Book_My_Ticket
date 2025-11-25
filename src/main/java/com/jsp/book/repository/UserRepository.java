89ba63f43a7d896ca1f44a430162a59c5bf0f269
anjali <anjalimsagar28@gmail.com>
2025-11-26 00:04:38 +0530

    Admin Registration Done

--- src/main/java/com/jsp/book/repository/UserRepository.java
+++ src/main/java/com/jsp/book/repository/UserRepository.java
@@ -6,4 +6,7 @@ import com.jsp.book.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+	boolean existsByEmail(String email);
+
+	void deleteByRole(String string);
 }
